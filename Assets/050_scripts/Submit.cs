b1a9bf1d074645483fa2d66335e29424f4d397cf
fezzlk <fezzlk796@gmail.com>
2021-08-15 02:19:14 +0900

    run python script and output result

---
+++ Assets/050_scripts/Submit.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Submit : MonoBehaviour
+{
+    //pythonがある場所
+    private string pyExePath = @"/Users/yusuketakizawa/opt/miniconda3/bin/python";
+
+    //実行したいスクリプトがある場所
+    private string pyCodePath = @"/Users/yusuketakizawa/mini_coder/Assets/050_scripts/pythonScript/run.py";
+
+
+    //オブジェクトと結びつける
+    public InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Componentを扱えるようにする
+        inputField = inputField.GetComponent<InputField>();
+    }
+
+    public void submitAnswer()
+    {
+        // テキストにinputFieldの内容を反映
+        // Debug.Log(inputField.text);
+        runPythonScript();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void runPythonScript()
+    {
+        //外部プロセスの設定
+        ProcessStartInfo processStartInfo = new ProcessStartInfo() {
+            FileName = pyExePath, //実行するファイル(python)
+            UseShellExecute = false,//シェルを使うかどうか
+            CreateNoWindow = true, //ウィンドウを開くかどうか
+            RedirectStandardOutput = true, //テキスト出力をStandardOutputストリームに書き込むかどうか
+            Arguments = pyCodePath + " '" + inputField.text + "'", //実行するスクリプト
+        };
+
+        //外部プロセスの開始
+        Process process = Process.Start(processStartInfo);
+
+        //ストリームから出力を得る
+        StreamReader streamReader = process.StandardOutput;
+        string str = streamReader.ReadLine();
+
+        //外部プロセスの終了
+        process.WaitForExit();
+        process.Close();
+
+        //実行
+        print(str);
+    }
+}
---
+++ Assets/050_scripts/pythonScript/run.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+
+    exec(f'print({str(sys.argv[1])})')
+
+
+if __name__ == '__main__':
+    main()
